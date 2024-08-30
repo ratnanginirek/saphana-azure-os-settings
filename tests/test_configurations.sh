dc3c533bb039e0a90d81bcf3acc0979dadf86b38
ratnanginirek <ratnanginirek@gmail.com>
2024-08-29 19:20:33 -0500

    Update test_configurations.sh

--- tests/test_configurations.sh
+++ tests/test_configurations.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# Test if sysctl settings are applied
+sysctl net.core.somaxconn
+sysctl net.ipv4.ip_local_port_range
+
+# Test if limits settings are applied
+grep "soft nproc" /etc/security/limits.conf
 
