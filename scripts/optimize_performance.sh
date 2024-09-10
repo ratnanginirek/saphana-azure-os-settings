7d67a168e28e3a1da81293626c87e865e4870f87
ratnanginirek <ratnanginirek@gmail.com>
2024-09-09 19:09:09 -0500

    Update optimize_performance.sh
    Add ANF settings

--- scripts/optimize_performance.sh
+++ scripts/optimize_performance.sh
@@ -1,1 +1,15 @@
-
+#ANF Optimization settings
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_rmem = 4096 131072 16777216
+net.ipv4.tcp_wmem = 4096 16384 16777216
+net.core.netdev_max_backlog = 300000
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_sack = 1
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv4.tcp_max_syn_backlog = 16348
+net.ipv4.conf.all.rp_filter = 0
+sunrpc.tcp_slot_table_entries = 128
